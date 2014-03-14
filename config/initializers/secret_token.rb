1a7444e0f1f85a41e0c90218943fe60287523654
Sean Hurley <sean.hurley6@gmail.com>
2014-03-14 15:46:09 -0500

    Initial commit - Initial server and opal installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sazh::Application.config.secret_key_base = 'df97e0b55c53206cec5cb7d4b7cedc91dd511472ca322c0f65aacc15d6852355d6ac33bea14669d4f853d738eb268d5bc519ec4a5c06787414cb9b983653f85d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sazh::Application.config.session_store :cookie_store, key: '_sazh_session'
